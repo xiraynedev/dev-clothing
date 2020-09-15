6644392505b4d0a68e4233014bfae267634b6797
xiraynedev <xiraynedev@gmail.com>
2020-09-15 12:46:33 -0400

    added components and styles to homepage

--- src/components/Homepage/Homepage.component.jsx
+++ src/components/Homepage/Homepage.component.jsx
@@ -1,40 +1,10 @@
 import React from 'react'
+import { DirectoryMenu } from '../DirectoryMenu/DirectoryMenu.component'
 import './Homepage.styles.scss'
 
 export const Homepage = () => (
   <div className='homepage'>
-    <div className='directory-menu'>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HATS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>JACKETS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>SNEAKERS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>MENS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>WOMENS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <DirectoryMenu />
   </div>
   
 )
