59bb41d3efa6d4051ab40bca799abc976eb6e51a
xiraynedev <xiraynedev@gmail.com>
2020-09-16 19:26:08 -0400

    adding header component with styles

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { Homepage } from './components/Homepage/Homepage.component'
 import { Route, Link, Switch } from 'react-router-dom'
 import { ShopPage } from './components/ShopPage/ShopPage.component';
+import { Header } from './components/Header/Header.component'
 
 function App() {
   return (
+    <>
+    <Header />
     <Switch>
       <Route exact path='/' component={Homepage} />
       <Route path='/shop' component={ShopPage} />
     </Switch>
-    
+    </>
   );
 }
 
---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.styles.scss'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <Link to='/' className='logo-container'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>SHOP</Link>
+        <Link className='option' to='/contact'>CONTACT</Link>
+      </div>
+    </div>
+  )
+}
