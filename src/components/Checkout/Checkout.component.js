cf3a8dd0fc85fb8c5df14dfcca9612a703bd0649
xiraynedev <xiraynedev@gmail.com>
2020-12-23 14:24:10 -0500

    Updating cart functionality and adding checkout page

--- src/components/Checkout/Checkout.component.js
+++ src/components/Checkout/Checkout.component.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from "reselect";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { selectCartTotal } from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../CheckoutItem/CheckoutItem.component";
 
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
@@ -25,7 +26,9 @@ const CheckoutPage = ({cartItems, total}) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem => cartItem.name)
+            cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))
         }
 
         <div className="total">
